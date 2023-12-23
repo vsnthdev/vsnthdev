150df5fdf7d86379c20ac4acd9e73448ebaa885c
Vasanth Srivatsa <vsnthdev@gmail.com>
2023-12-23 13:49:45 +0530

    ✨ create random catchy lines while loading data

--- src/fetchProfile.ts
+++ src/fetchProfile.ts
@@ -4,6 +4,7 @@
  */
 
 import ora from 'ora'
+import chalk from 'chalk'
 
 interface Profile {
     email: string
@@ -15,10 +16,23 @@ interface Profile {
 
 export let profile: Profile
 
+const h = chalk.whiteBright.bold
+const d = chalk.dim
+
+const lines = [
+    `${h('Whispers of your wizardry echo through the terminal')} 🧙‍♂️ ${d('prepare to be dazzled')} 🪄`,
+    `${h('Channeling the magic of coding, servers & internet')} 🌏 ${d('brace yourself')} 🙌`,
+    `${h('Summoning spells of brilliance, line by line')} ✨`,
+    `${h('Loading incantations of experience, brace yourself')} 🚀 ${d('for a magical portfolio')} ⚡️`,
+    `${d('Brewing a concoction ☕️ of spells and skills')}${d(', loading an arcane portfolio')} 👏`,
+]
+
+const getRandomElement = <T>(arr: T[]): T => arr[Math.floor(Math.random() * arr.length)];
+
 export async function fetchProfile() {
     const spinner = ora({
         color: 'cyan',
-        text: 'Loading'
+        text: getRandomElement(lines)
     })
 
     console.clear()
